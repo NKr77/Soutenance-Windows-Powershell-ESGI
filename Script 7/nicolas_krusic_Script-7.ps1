d0065f282b6d7d2622115f6247c577658e154a39
NKr77 <krusicnicolas@gmail.com>
2026-07-06 12:47:31 +0200

    correction script 7

--- Script 7/nicolas_krusic_Script-7.ps1
+++ Script 7/nicolas_krusic_Script-7.ps1
@@ -2,5 +2,5 @@ $fichierCsv = "deplacerUsersDansGroupes.csv"
 $donnees = Import-Csv -Path $fichierCsv -Delimiter ";"
 
 foreach ($ligne in $donnees) {
-    Add-LocalGroupMember -Group $ligne.Groupe -Member $ligne.Utilisateur
+    Add-LocalGroupMember -Group $ligne.nomGroupe -Member $ligne.nomUser
 }
